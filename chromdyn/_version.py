3c03bb409d01effebd77ee18d78fc0cf557e48d4
s-brahmachari <b.sumitabha@gmail.com>
2025-12-09 05:59:08 -0600

    fix pypi

--- chromdyn/_version.py
+++ chromdyn/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post75'
-__version_tuple__ = version_tuple = (0, 0, 'post75')
+__version__ = version = '0.0.post77'
+__version_tuple__ = version_tuple = (0, 0, 'post77')
 
-__commit_id__ = commit_id = 'g0bf0e29cb'
+__commit_id__ = commit_id = 'g4ac085194'
